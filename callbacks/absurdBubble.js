74f59829883e707809357dfa1c0ae6b27121682d
will <will.kasten@gmail.com>
2018-09-21 16:51:00 -0700

    finish absurdBubble.js

--- callbacks/absurdBubble.js
+++ callbacks/absurdBubble.js
@@ -9,7 +9,14 @@ reader = readline.createInterface({
 
 function absurdBubbleSort(arr, sortCompletionCallback) {
 
-
+  function outerBubbleSortLoop(swaps) {
+    if (swaps === true) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
 }
 
 function askIfGreaterThan(el1, el2, cb) {
@@ -22,29 +29,36 @@ function askIfGreaterThan(el1, el2, cb) {
   });
 }
 
-function trueFalse(bool) {
-  if (bool) {
-    console.log("True, sucka!");
-  } else {
-    console.log("False, try again.... sucka.");
-  }
-  reader.close();
-}
-
-// askIfGreaterThan(5, 7, trueFalse);
-
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+
   if (i < arr.length - 1) {
+
     askIfGreaterThan(arr[i], arr[i + 1], function(isGreaterThan) {
+
       if (isGreaterThan) {
+
         let first = arr[i];
         let second = arr[i+1];
         arr[i] = second;
         arr[i+1] = first;
-        
+        madeAnySwaps = true;
+
       }
+      console.log(arr);
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+
+
     });
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
   }
 }
 
-innerBubbleSortLoop([1, 2, 3], 0, false);
+function dummy(arr) {
+  reader.close();
+  console.log(`Sorted array: ${arr}`);
+  return arr;
+}
+// innerBubbleSortLoop([2,5,1,4,3], 0, false, outerBubbleSortLoop);
+
+absurdBubbleSort([3,2,1,4], dummy);
